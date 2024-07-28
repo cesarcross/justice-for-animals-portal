4b8d4f6ddef5d70821858163e93f6fa08692e546
Cesar Augusto Corregiari Filho <ccorregi@its.jnj.com>
2024-07-28 17:24:34 -0300

    fix comma at colors.js

--- src/utils/constants/colors.js
+++ src/utils/constants/colors.js
@@ -1,8 +1,8 @@
 export const COLORS = {
-dark: "#001524",
-green: "#445D48",
-yellow: "#D6CC99",
-pink: "#FDE5D4"
-red: "#EE4E4E",
-white: "#fff",
+  dark: "#001524",
+  green: "#445D48",
+  yellow: "#D6CC99",
+  pink: "#FDE5D4",
+  red: "#EE4E4E",
+  white: "#fff",
 };
